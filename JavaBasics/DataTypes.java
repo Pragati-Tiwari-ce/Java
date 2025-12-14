29b67a969eee13720c5a933238586ac17f753212
Pragati-Tiwari-ce <tiwaripragati2005@gmail.com>
2025-12-14 16:16:54 +0530

    Create DataTypes.java

---
+++ JavaBasics/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean var = true;
+        System.out.println(var);
+
+        float price = 7.867f;
+        System.out.println(price);
+
+        int number = 756;
+        System.out.println(number);
+
+        double num = 77.8999999;
+        System.out.println(num);
+
+        long n = 678667777L;
+        System.out.println(n);
+    }
+}
