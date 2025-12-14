b3501e8927104db6573272fc7e4b220520ae49b5
Pragati-Tiwari-ce <tiwaripragati2005@gmail.com>
2025-12-14 15:54:32 +0530

    Add files via upload

---
+++ JavaBasics/variables.java
@@ -0,0 +1,11 @@
+public class variables{
+    public static void main(String args[]){
+        int a=5;
+        int b=10;
+        System.out.println(a);
+        String name="Tony";
+        System.out.println(name);
+
+
+    }
+}
