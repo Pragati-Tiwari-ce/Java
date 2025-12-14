de722b6fe61a620e3497368dd00f76d43ea5e533
Pragati-Tiwari-ce <tiwaripragati2005@gmail.com>
2025-12-14 16:24:14 +0530

    Create Sum.java
    Added Java program to calculate sum of two integers

---
+++ JavaBasics/Sum.java
@@ -0,0 +1,8 @@
+public class Sum{
+    public static void main(String args[]){
+        int a=5;
+        int b=6;
+        int sum=a+b;
+        System.out.println("Sum of a and b is: "+sum);
+    }
+}
