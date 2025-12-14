7e0f1ea910d59532642195880ca4c42251cd8138
Pragati-Tiwari-ce <tiwaripragati2005@gmail.com>
2025-12-14 15:31:07 +0530

    Create HelloWorld.java

---
+++ JavaBasics/HelloWorld.java
@@ -0,0 +1,8 @@
+public class code{
+    public static void main(String args[]){
+        System.out.println("how are you");
+        System.out.print("how are you\n");
+        System.out.println("how are you");
+
+    }
+}
