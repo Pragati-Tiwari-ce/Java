9f4a1f3937c2860cbe0ce5990bc9ca449c78cbf3
Pragati-Tiwari-ce <tiwaripragati2005@gmail.com>
2025-12-14 16:28:43 +0530

    Create Comments.java

---
+++ JavaBasics/Comments.java
@@ -0,0 +1,15 @@
+public class Comments{
+    public static void main(String args[]){
+      
+        //hi there!this is single line comment
+      
+        /*
+        hie
+        this 
+        is 
+        a multiline
+        comment
+        */
+        System.out.println("comments in java");
+    }
+}
