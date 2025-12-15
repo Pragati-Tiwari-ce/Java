bda6d0cdd5eba12fbd3afd99811fed69f4da8129
Pragati-Tiwari-ce <tiwaripragati2005@gmail.com>
2025-12-15 14:03:49 +0530

    Create CompareNumbers.java

---
+++ JavaBasics/CompareNumbers.java
@@ -0,0 +1,14 @@
+public class CompareNumbers {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 3;
+
+        if (a > b) {
+            System.out.println("A is greater");
+        } else if (a < b) {
+            System.out.println("B is greater");
+        } else {
+            System.out.println("Both are equal");
+        }
+    }
+}
