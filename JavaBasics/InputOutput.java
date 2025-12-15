b039dd6ef75d33698f433e15aa5afbcfc20aa70b
Pragati-Tiwari-ce <tiwaripragati2005@gmail.com>
2025-12-15 11:07:37 +0530

    Create InputOutput.java

---
+++ JavaBasics/InputOutput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class InputOutput{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+      
+        boolean input=sc.nextBoolean();
+        System.out.println(input);
+      
+        int a=sc.nextInt();
+        System.out.println(a);
+    }
+}
