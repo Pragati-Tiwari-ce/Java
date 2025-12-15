3aaf5a8cba84c2390dc48dd8a2cbadd1534b6a85
Pragati-Tiwari-ce <tiwaripragati2005@gmail.com>
2025-12-15 12:42:27 +0530

    Create ArithmeticOperators.java
    Added Java program demonstrating arithmetic operators

---
+++ JavaBasics/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ArithmeticOperators{
+    public static void main(String args[]){
+
+       int a=10;
+       int b=5;
+       int sum= a+b;
+       int subtraction=a-b;
+       int mul=a*b;
+       double div=a/b;
+       int rem=a%b;
+
+       System.out.println("addition: "+sum);
+       System.out.println("subtraction: " +subtraction);
+       System.out.println("product: "+mul);
+       System.out.println("division is: "+div);
+       System.out.println("remainder is: "+rem);  
+    }
+}
